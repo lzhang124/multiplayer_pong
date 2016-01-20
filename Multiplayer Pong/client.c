2edec19d7ff7c92f8fb80fc279f9f1229357b13f
Rose Wang <rzwang@mit.edu>
2016-01-20 15:56:54 -0500

    added server.c and client.c files

---
+++ Multiplayer Pong/client.c
@@ -0,0 +1,9 @@
+//
+//  client.c
+//  Multiplayer Pong
+//
+//  Created by Rose Wang on 1/20/16.
+//
+//
+
+#include <stdio.h>
---
+++ Multiplayer Pong/server.c
@@ -0,0 +1,9 @@
+//
+//  server.c
+//  Multiplayer Pong
+//
+//  Created by Rose Wang on 1/20/16.
+//
+//
+
+#include <stdio.h>
