3322040c703de8f540566382b758229c729384b1
Rose Wang <rzwang@mit.edu>
2016-01-20 18:09:49 -0500

    not working -- trying to get rid of argc

--- Server/main.c
+++ Server/main.c
@@ -1,11 +1,9 @@
 //
-//  main.c
-//  Server
+//  server.c
+//  Multiplayer Pong
 //
 //
 
-/* A simple server in the internet domain using TCP
- The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,48 +12,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg)
+//const int serverPort = 9000;
+
+void server()
 {
-    perror(msg);
-    exit(1);
+    // open socket
+    int sockfd;
+    sockfd = socket(AF_INET6, SOCK_STREAM, 0); // address domain, socket type, protocol
+    if (sockfd < 0)
+        perror("Error opening socket");
+    
+    // set up server address and port
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET6;
+    server_addr.sin_port = htons(9000);
+    server_addr.sin_addr.s_addr = INADDR_ANY; // IP address of host aka IP address of my machine
+    
+    // bind socket to server address + serverPort
+    if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+        perror("Error binding socket");
+    
+    // listen
+    listen(sockfd, 5); // listen on socket for at most 5 backlog queue
+    
+    // accept incoming connection, need client address
+    struct sockaddr_in cli_addr;
+
+    int newsocketfd = accept(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr));
+    while (newsocketfd > 0)
+    {
+        // read from socket into buffer
+        char buffer[256];
+        bzero(buffer, 256);
+        int n = read(newsocketfd, buffer, 255);
+        if (n < 0)
+            perror("Error in reading");
+        
+        close(newsocketfd);
+        newsocketfd = accept(sockfd, (struct sockaddr *) &cli_addr, sizeof(cli_addr));
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, newsockfd, portno;
-    socklen_t clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-    if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0)
-        error("ERROR on binding");
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd,
-                       (struct sockaddr *) &cli_addr,
-                       &clilen);
-    if (newsockfd < 0)
-        error("ERROR on accept");
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
-    if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    n = write(newsockfd,"I got your message",18);
-    if (n < 0) error("ERROR writing to socket");
-    close(newsockfd);
-    close(sockfd);
+    server();
     return 0;
 }
+
+//struct sockaddr_in
+//{
+//    short   sin_family; /* must be AF_INET */
+//    u_short sin_port;
+//    struct  in_addr sin_addr;
+//    char    sin_zero[8]; /* Not used, must be zero */
+//};
