b27947ac69c3c2dbb8d90199d5cc6cc7e548a341
Larry <lzhang124@gmail.com>
2016-01-21 16:10:00 -0500

    server.c/h and pong.c/h

---
+++ Server/server.h
@@ -0,0 +1,21 @@
+//
+//  server.h
+//  Multiplayer Pong
+//
+//
+
+#ifndef server_h
+#define server_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
+void server();
+
+#endif /* server_h */
