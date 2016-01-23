84f1bbaa5ef9ae9ff8afb8548002efb6287268a1
Rose Wang <rzwang@mit.edu>
2016-01-23 17:19:07 -0500

    got back constants.h

---
+++ Server/constants.h
@@ -0,0 +1,14 @@
+//
+//  constants.h
+//  Multiplayer Pong
+//
+//  Created by Rose Wang on 1/23/16.
+//
+//
+
+#ifndef constants_h
+#define constants_h
+
+#endif constants_h
+
+#define MAX_PLAYERS 4
