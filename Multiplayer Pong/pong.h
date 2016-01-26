31f2a0d0494e59b72ca04694fe3fca3d4bd439db
Larry <lzhang124@gmail.com>
2016-01-26 15:09:30 -0500

    modifying includes

--- Multiplayer Pong/pong.h
+++ Multiplayer Pong/pong.h
@@ -17,9 +17,8 @@
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
-#include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
-#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
 #else
 #include <GL/glut.h>
 #include <GL/glu.h>
